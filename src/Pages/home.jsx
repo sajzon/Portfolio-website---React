3dfe60c86b724b5fb081d491f7aed3fc764802e3
sajzon <sajithsandakelum@gmail.com>
2023-11-09 02:26:52 +0530

    sidebar commit

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -5,16 +5,16 @@ import Test from '../Test'
 const home = () => {
   return (
     <div>
-      <section>
+      <section id='Homepage'>
         <Navbar/>
       </section>
-      <section>parallax</section>
+      <section id='Services'>parallax</section>
       <section>services</section>
-      <section>parallax</section>
+      <section id='Portfolio'>parallax</section>
       <section>portfolio1</section>
       <section>portfolio2</section>
       <section>portfolio3</section>
-      <section>contact</section>
+      <section id='Contact'>contact</section>
       {/* <Test/> */}
     </div>
   )

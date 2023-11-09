e056937219aa1af6559c3bb72308c10afd68ed83
sajzon <sajithsandakelum@gmail.com>
2023-11-10 03:50:43 +0530

    full home page

--- src/Pages/home.jsx
+++ src/Pages/home.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import "./homeView.css"
 import Navbar from '../Components/navbar/Navbar'
 import Test from '../Test'
+import Hero from '../Components/hero/Hero'
 const home = () => {
   return (
     <div>
       <section id='Homepage'>
         <Navbar/>
+        <Hero/>
       </section>
       <section id='Services'>parallax</section>
       <section>services</section>
